0ba901eefc4cacfb3c84cc6ad0fee3b8e511aaec
KIIT <asstore3@gmail.com>
2020-07-09 00:54:44 +0530

    Done with card and listtile

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,76 +16,69 @@ class MyApp extends StatelessWidget {
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
-               backgroundImage: AssetImage('resources/pp.jpg'),
+                backgroundImage: AssetImage('resources/pp.jpg'),
               ),
               Text(
                 'Aayush Sahay',
                 style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontFamily: 'Pacifico',
-                  letterSpacing: 2.0
-                ),
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Pacifico',
+                    letterSpacing: 2.0),
               ),
               Text(
                 'Flutter Developer',
                 style: TextStyle(
                   fontFamily: 'SourceSansPro',
-                      fontSize: 25,
+                  fontSize: 25,
                   color: Colors.white,
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.only(left: 27,right: 27),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      size: 20,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      '7903676591',
-                      style: TextStyle(
-                        fontSize: 17,
-                      ),
-                    ),
-                  ],
+              SizedBox(
+                height: 30,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
                 ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.only(left: 27, right: 27),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 20,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '123678123',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                )
               ),
               SizedBox(
                 height: 20,
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              Card(
                 color: Colors.white,
-                margin: EdgeInsets.only(left: 27,right: 27),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      size: 20,
-                      color: Colors.teal,
+                margin: EdgeInsets.only(left: 27, right: 27),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 20,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'aayush@emailaddress.com',
+                    style: TextStyle(
+                      fontSize: 17,
                     ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'asstore3@gmail.com',
-                      style: TextStyle(
-                        fontSize: 17,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-
+                  ),
+                )
+              ),
             ],
           ),
         ),
